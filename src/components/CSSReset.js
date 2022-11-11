a872f01804a1371fa572b952cd1498cc91f53732
Igor Marques <soigormarques@gmail.com>
2022-11-11 14:37:41 -0300

    first commit

---
+++ src/components/CSSReset.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from "styled-components";
+
+export const CSSReset = createGlobalStyle`
+  /* Reset */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: sans-serif;
+    background-color: ${({ theme }) => theme.backgroundBase};
+    color: ${({ theme }) => theme.textColorBase};
+  }
+  /* NextJS */
+  html {
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+  }
+  html::-webkit-scrollbar-track {
+    background-color: #202020;
+  }
+  html::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background: #202020;
+  }
+  html::-webkit-scrollbar-thumb {
+    background: #404040;
+  }
+  body {
+    display: flex;
+    flex: 1;
+  }
+  #__next {
+    display: flex;
+    flex: 1;
+  }
+  /* Globals */
+  button,
+  a {
+    text-decoration: none;
+    opacity: 1;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+  }
+`;
